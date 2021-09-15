2f844285e24dd6ccd01086af9b952e988936aec7
Ritika Garg <ritikarg22@gmail.com>
2021-09-15 13:24:57 +0530

    navigation package added

---
+++ src/config/navigator.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { BackHandler } from 'react-native';
+import { shape, string, number, func } from 'prop-types';
+import routes from './routes';
+
+class navigator extends React.Component {
+
+}
+
+export default navigator;
